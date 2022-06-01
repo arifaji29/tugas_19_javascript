267d8f2bef105ff63fb0f7f0f611487449b87111
Arif <arifajif@gmail.com>
2022-06-01 22:43:12 +0700

    tugas 19

---
+++ tugas19.js
@@ -0,0 +1,21 @@
+function ulang() {
+    var isi = "Semangat...";
+    console.log(isi.repeat(5));
+    console.log("================================");
+
+}
+ulang();
+
+function ulang2(){
+    var isi2 = "*";
+    console.log(isi2.repeat(5));
+    console.log(isi2.repeat(25));
+    console.log(isi2.repeat(55));
+    console.log(isi2.repeat(75));
+    console.log(isi2.repeat(105));
+    console.log(isi2.repeat(75));
+    console.log(isi2.repeat(55));
+    console.log(isi2.repeat(25));
+    console.log(isi2.repeat(5));
+}
+ulang2();
